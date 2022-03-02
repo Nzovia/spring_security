7715a43c6a5537373ec9603d01ccaee04d6d9843
Nzovia <nickiemaundu19@gmail.com>
2022-03-02 11:45:51 +0300

    updated controller

--- src/main/java/com/nicholas/springsecuritywithjwt/api/UserController.java
+++ src/main/java/com/nicholas/springsecuritywithjwt/api/UserController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class UserController {
     public ResponseEntity<List<AppUser>> getUsers(){
         //calling the service to return users from the DB
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    //post data to the server
+    @GetMapping("/users")
+    public ResponseEntity<AppUser> saveUsers(@RequestBody AppUser appUser){
+        return ResponseEntity.ok().body(userService.saveAppUser(appUser));
     }
 }
